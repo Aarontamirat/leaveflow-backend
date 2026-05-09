9a94d4a9e10af70d9661dee0b77f9a0edece0f74
Aaron Tamirat <AaronT@LUCY.COM>
2026-05-09 16:06:21 +0300

    Completed

--- src/app.js
+++ src/app.js
@@ -11,12 +11,7 @@ const leaveRoutes = require("./routes/leaveRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 // Swagger Docs
