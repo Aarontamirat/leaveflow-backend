7e6d0a214ddcc578f508eca42710e0cb917378db
Aaron Tamirat <AaronT@LUCY.COM>
2026-05-13 08:22:34 +0300

    Auto commit on 2026-05-13 08:22:34

--- src/app.js
+++ src/app.js
@@ -19,12 +19,7 @@ const limiter = rateLimit({
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(limiter);
 app.use(helmet());
 app.use(express.json());
